ae65bfc5e6f1d80ae25b19acaabc0122c1a61eba
17sTomy <tomasignacioalv@hotmail.com>
2023-08-01 23:49:20 -0300

    agrego de campo de mail al registrarse

--- Autenticacion/views.py
+++ Autenticacion/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # Create your views here.
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField() 
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+
 class VRegistro(View):
-    
     def get(self, request):
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
         return render(request, "registro/registro.html", {"form": form})
     
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             usuario = form.save()
             login(request, usuario)
