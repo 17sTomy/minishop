e04673387957a164d3ba5a1973f91cfa76b9616e
17sTomy <tomasignacioalv@hotmail.com>
2023-08-01 22:44:28 -0300

    crea superusuario automaticamente

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
